4a8c29e22328424aeb7d1c70e9c63898f0a5e97c
unknown <tamieamakiri@gmail.com>
2022-06-29 15:32:59 +0100

    Service Popup

---
+++ script.js
@@ -0,0 +1,50 @@
+const pop = document.querySelector('.pop');
+const services = document.querySelector('.services')
+const service_div = document.querySelectorAll('.services div');
+const service_popup = document.querySelectorAll('.service-popup');
+const service_icon = document.querySelectorAll('.service-popup icon');
+
+services.addEventListener('click', popup);
+
+
+function popup() {
+    if (service_div[0]){
+        // remove hide class from pop
+        pop.classList.remove('hide');
+
+        // add hide class to saervice popup
+        service_popup[1].classList.add('hide');
+        service_popup[2].classList.add('hide');
+        service_popup[3].classList.add('hide');
+    } else if (service_div[1]) {
+        // remove hide class from pop
+        pop.classList.remove('hide');
+
+        // add hide class to saervice popup
+        service_popup[0].classList.add('hide');
+        service_popup[2].classList.add('hide');
+        service_popup[3].classList.add('hide');
+    } else if (service_div[2]) {
+        // remove hide class from pop
+        pop.classList.remove('hide');
+
+        // add hide class to saervice popup
+        service_popup[1].classList.add('hide');
+        service_popup[0].classList.add('hide');
+        service_popup[3].classList.add('hide');
+    } else if (service_div[3]) {
+        // remove hide class from pop
+        pop.classList.remove('hide');
+
+        // add hide class to saervice popup
+        service_popup[1].classList.add('hide');
+        service_popup[2].classList.add('hide');
+        service_popup[0].classList.add('hide');
+    } else {
+        // remove hide class from pop
+        pop.classList.add('hide');
+
+    }
+}
+
+console.log(service_icon)
